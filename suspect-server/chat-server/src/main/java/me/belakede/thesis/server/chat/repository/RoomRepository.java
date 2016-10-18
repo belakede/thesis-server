3a365c48b141087e38c2c28256fdc0d83527accd
belakede <belakede@gmail.com>
2016-10-18 22:00:19 +0200

    Add findAll() to RoomRepository and RoomService
    (cherry picked from commit fa2de9f)

--- suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/repository/RoomRepository.java
+++ suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/repository/RoomRepository.java
@@ -4,9 +4,12 @@ import me.belakede.thesis.server.chat.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    List<Room> findAll();
     Room findByName(String name);
 
 }
--- suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/service/RoomService.java
+++ suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/service/RoomService.java
@@ -5,6 +5,7 @@ import me.belakede.thesis.server.chat.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,6 +20,10 @@ public class RoomService {
 
     public Room create() {
         return repository.save(new Room(UUID.randomUUID().toString()));
+    }
+
+    public List<Room> findAll() {
+        return repository.findAll();
     }
 
     public Room findByName(String name) {
