5435580c76f55b06ab5d936cec063420b6f1c60f
Adam Belak <belakede@gmail.com>
2016-10-19 16:07:22 +0200

    Create EmitterService
    (cherry picked from commit 644b641)

---
+++ suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/service/EmitterService.java
@@ -0,0 +1,44 @@
+package me.belakede.thesis.server.chat.service;
+
+import me.belakede.thesis.server.chat.domain.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmitterService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmitterService.class);
+    private final List<SseEmitter> emitters;
+
+    public EmitterService() {
+        this.emitters = new ArrayList<>();
+    }
+
+    public SseEmitter createEmitter() {
+        SseEmitter emitter = new SseEmitter();
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitters.add(emitter);
+        return emitter;
+    }
+
+    public void broadcast(Message message) {
+        emitters.forEach((SseEmitter emitter) -> {
+            try {
+                emitter.send(message, MediaType.APPLICATION_JSON);
+            } catch (IOException e) {
+                emitter.complete();
+                emitters.remove(emitter);
+                LOGGER.warn("Can't broadcast the message for emitter {} - Connection closed.", emitter);
+                LOGGER.warn("{}", e);
+            }
+        });
+    }
+
+}
