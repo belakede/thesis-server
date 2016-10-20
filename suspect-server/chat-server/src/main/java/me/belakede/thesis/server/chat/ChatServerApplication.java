cc1333e7146b558d6d6453cc1d7963b77ff22759
Adam Belak <belakede@gmail.com>
2016-10-20 15:07:56 +0200

    Use chat-server.properties instead of application.yml
    (cherry picked from commit 9a2f2fb)

--- suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/ChatServerApplication.java
+++ suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/ChatServerApplication.java
@@ -2,8 +2,10 @@ package me.belakede.thesis.server.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("chat-server.properties")
 public class ChatServerApplication {
 
     public static void main(String[] args) {
