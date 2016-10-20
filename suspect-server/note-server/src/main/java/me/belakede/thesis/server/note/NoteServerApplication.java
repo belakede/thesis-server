09059fee1d365bea39ce0a69056915a142509a91
Adam Belak <belakede@gmail.com>
2016-10-20 15:26:54 +0200

    Use note-server.properties instead of application.yml
    (cherry picked from commit b4ca1de)

--- suspect-server/note-server/src/main/java/me/belakede/thesis/server/note/NoteServerApplication.java
+++ suspect-server/note-server/src/main/java/me/belakede/thesis/server/note/NoteServerApplication.java
@@ -2,8 +2,10 @@ package me.belakede.thesis.server.note;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("note-server.properties")
 public class NoteServerApplication {
 
     public static void main(String[] args) {
