c837175d98ccd00bd92ac37a920ed3caeec5f6e6
Adam Belak <belakede@gmail.com>
2016-10-21 16:42:20 +0200

    Create User constructors
    (cherry picked from commit 270ffcc)

--- suspect-server/auth-server/src/main/java/me/belakede/thesis/server/auth/domain/User.java
+++ suspect-server/auth-server/src/main/java/me/belakede/thesis/server/auth/domain/User.java
@@ -25,6 +25,16 @@ public class User {
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     private Set<Role> roles;
 
+    public User() {
+    }
+
+    public User(String username, String password, boolean enabled, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
+
     public Long getId() {
         return id;
     }
