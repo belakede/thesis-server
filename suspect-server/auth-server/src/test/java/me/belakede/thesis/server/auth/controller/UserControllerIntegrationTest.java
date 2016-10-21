86079734289b936c3af82dd2bd761199c08949ca
Adam Belak <belakede@gmail.com>
2016-10-21 17:23:13 +0200

    Add integration test for UserController
    (cherry picked from commit 9d3cc39)

---
+++ suspect-server/auth-server/src/test/java/me/belakede/thesis/server/auth/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,101 @@
+package me.belakede.thesis.server.auth.controller;
+
+import me.belakede.junit.OrderedSpringRunner;
+import me.belakede.thesis.server.auth.repository.UserRepository;
+import me.belakede.thesis.server.test.OAuthHelper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(OAuthHelper.class)
+@RunWith(OrderedSpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerIntegrationTest {
+
+    private static final String TEST_USER = "testuser1";
+
+    @Autowired
+    private UserRepository repository;
+    @Value("${suspect.server.admin.username}")
+    private String adminUsername;
+
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private OAuthHelper authHelper;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @Order(1)
+    public void getUsersShouldReturnAnErrorMessageWhenUserIsNotAuthenticated() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/users")).andDo(print());
+        resultActions.andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @Order(2)
+    public void createUserShouldReturnWithTheCreatedUser() throws Exception {
+        RequestPostProcessor bearerToken = authHelper.addBearerToken(repository.findByUsername(adminUsername));
+        ResultActions resultActions = mockMvc.perform(post("/users").with(bearerToken).param("username", TEST_USER).param("password", "password")).andDo(print());
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"username\":\"" + TEST_USER + "\"")))
+                .andExpect(content().string(containsString("\"roles\":[\"USER\"]")))
+                .andExpect(content().string(containsString("\"enabled\":true")));
+    }
+
+    @Test
+    @Order(3)
+    public void getUsersShouldReturnAListWithAllRegisteredUsersWithRoleUser() throws Exception {
+        RequestPostProcessor bearerToken = authHelper.addBearerToken(repository.findByUsername(adminUsername));
+        ResultActions resultActions = mockMvc.perform(get("/users").with(bearerToken)).andDo(print());
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(not(containsString("\"username\":\"admin\""))))
+                .andExpect(content().string(containsString("\"username\":\"" + TEST_USER + "\"")));
+    }
+
+    @Test
+    @Order(4)
+    public void removeUserShouldReturnWithTheRemovedUser() throws Exception {
+        ResultActions resultActions;
+        RequestPostProcessor bearerToken = authHelper.addBearerToken(repository.findByUsername(adminUsername));
+        resultActions = mockMvc.perform(delete("/users").with(bearerToken).param("username", TEST_USER)).andDo(print());
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"username\":\"" + TEST_USER + "\"")))
+                .andExpect(content().string(containsString("\"roles\":[\"USER\"]")))
+                .andExpect(content().string(containsString("\"enabled\":true")));
+        resultActions = mockMvc.perform(get("/users").with(bearerToken)).andDo(print());
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(not(containsString("\"username\":\"" + TEST_USER + "\""))));
+    }
+
+}
