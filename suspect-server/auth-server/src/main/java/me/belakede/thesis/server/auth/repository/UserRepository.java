60907978de66ade83abf4456ac9dc5bbfa8beeec
Adam Belak <belakede@gmail.com>
2016-10-21 15:41:25 +0200

    Add findByRolesContains(Role) method
    (cherry picked from commit 87cba6e)

--- suspect-server/auth-server/src/main/java/me/belakede/thesis/server/auth/repository/UserRepository.java
+++ suspect-server/auth-server/src/main/java/me/belakede/thesis/server/auth/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package me.belakede.thesis.server.auth.repository;
 
+import me.belakede.thesis.server.auth.domain.Role;
 import me.belakede.thesis.server.auth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
+
+    List<User> findByRolesContains(Role role);
+
 }
