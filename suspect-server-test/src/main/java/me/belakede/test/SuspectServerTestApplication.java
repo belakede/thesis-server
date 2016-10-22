afe3d846920b92aa897a6711309a3b73c9ac9e96
belakede <belakede@gmail.com>
2016-10-22 23:00:50 +0200

    Add OrderedSpringRunner and OAuth2Helper classes
    (cherry picked from commit b95af84)

---
+++ suspect-server-test/src/main/java/me/belakede/test/SuspectServerTestApplication.java
@@ -0,0 +1,12 @@
+package me.belakede.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuspectServerTestApplication {
+
+    public static void main(String[] args) {
+        //
+    }
+
+}
---
+++ suspect-server-test/src/main/java/me/belakede/test/security/oauth2/OAuth2Helper.java
@@ -0,0 +1,40 @@
+package me.belakede.test.security.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+@Component
+public class OAuth2Helper {
+
+    @Autowired
+    AuthorizationServerTokenServices tokenservice;
+
+    @Value("${oauth2.client.id}")
+    private String clientId;
+
+    @Value("${oauth2.client.secret}")
+    private String secret;
+
+    public RequestPostProcessor addBearerToken(String username, String... authorities) {
+        return mockRequest -> {
+            mockRequest.addHeader("Authorization", "Bearer " + createOAuth2AccessToken(username, authorities).getValue());
+            return mockRequest;
+        };
+    }
+
+    private OAuth2AccessToken createOAuth2AccessToken(String username, String[] authorities) {
+        OAuth2Request oauth2Request = new OAuth2Request(null, clientId, null, true, null, null, null, null, null);
+        Authentication userauth = new TestingAuthenticationToken(username, null, authorities);
+        OAuth2Authentication oauth2auth = new OAuth2Authentication(oauth2Request, userauth);
+        return tokenservice.createAccessToken(oauth2auth);
+    }
+
+}
