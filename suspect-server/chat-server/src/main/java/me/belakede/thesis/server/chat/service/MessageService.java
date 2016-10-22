b1951e789718b4a074b93e60ee53f22b7a4bef99
belakede <belakede@gmail.com>
2016-10-22 11:16:15 +0200

    Throw MissingSenderException when sender is null
    (cherry picked from commit cf0286f)

--- suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/service/MessageService.java
+++ suspect-server/chat-server/src/main/java/me/belakede/thesis/server/chat/service/MessageService.java
@@ -2,12 +2,16 @@ package me.belakede.thesis.server.chat.service;
 
 import me.belakede.thesis.server.chat.domain.Message;
 import me.belakede.thesis.server.chat.domain.Sender;
+import me.belakede.thesis.server.chat.exception.MissingSenderException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
 
-    public Message create(Sender sender, String message) {
+    public Message create(Sender sender, String message) throws MissingSenderException {
+        if (sender == null) {
+            throw new MissingSenderException("The specified sender is not valid.");
+        }
         return new Message(sender.getName(), message);
     }
 }
