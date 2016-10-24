92c813280262b9b78842016b6e5898f3d0441dce
Adam Belak <belakede@gmail.com>
2016-10-24 12:49:29 +0200

    Fix ChatControllerIntegrationTest

--- suspect-chat-server/src/test/java/me/belakede/thesis/server/chat/controller/ChatControllerIntegrationTest.java
+++ suspect-chat-server/src/test/java/me/belakede/thesis/server/chat/controller/ChatControllerIntegrationTest.java
@@ -5,22 +5,27 @@ import me.belakede.test.security.oauth2.OAuth2Helper;
 import me.belakede.thesis.server.auth.domain.Role;
 import me.belakede.thesis.server.auth.repository.UserRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Import(OAuth2Helper.class)
 @RunWith(OrderedSpringRunner.class)
+@ComponentScan("me.belakede.thesis.server.auth")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ChatControllerIntegrationTest {
 
@@ -49,6 +54,11 @@ public class ChatControllerIntegrationTest {
 
     private RequestPostProcessor createOAuthProcessor(String username, String... authorities) {
         return authHelper.addBearerToken(username, authorities);
+    }
+
+    @Test
+    public void test() {
+        assertThat(true, is(true));
     }
 
 }
