712192646e4fe8b9d71946d2b78b36b432c6107f
Adam Belak <belakede@gmail.com>
2016-10-26 16:02:56 +0200

    Add missing Equals test for User

--- suspect-auth-server/src/test/java/me/belakede/thesis/server/auth/domain/UserTest.java
+++ suspect-auth-server/src/test/java/me/belakede/thesis/server/auth/domain/UserTest.java
@@ -1,11 +1,16 @@
 package me.belakede.thesis.server.auth.domain;
 
 import me.belakede.junit.ExtendedEqualsHashCodeTestCase;
+import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.HashSet;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(Enclosed.class)
 public class UserTest {
@@ -24,6 +29,15 @@ public class UserTest {
         @Override
         protected Object createNotEqualInstance() throws Exception {
             return new User("user", "password", true, new HashSet<>(Arrays.asList(Role.USER)));
+        }
+
+        @Test
+        public void testEqualsShouldReturnFalseWhenEnabledFieldIsOpposite() throws Exception {
+            User user = (User) createInstance();
+            User otherUser = (User) createInstance();
+            otherUser.setEnabled(false);
+            assertThat(user, not(equalTo(otherUser)));
+            assertThat(otherUser, not(equalTo(user)));
         }
     }
 
