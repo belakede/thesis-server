7e2ae1e9428029d43ae8b65c7c7c8611b0401908
Adam Belak <belakede@gmail.com>
2016-10-26 15:54:41 +0200

    Remove unnecessary constructors

--- suspect-auth-server/src/main/java/me/belakede/thesis/server/auth/exception/MissingUserException.java
+++ suspect-auth-server/src/main/java/me/belakede/thesis/server/auth/exception/MissingUserException.java
@@ -2,14 +2,8 @@ package me.belakede.thesis.server.auth.exception;
 
 public class MissingUserException extends Exception {
 
-    public MissingUserException() {
-    }
-
     public MissingUserException(String message) {
         super(message);
     }
 
-    public MissingUserException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
