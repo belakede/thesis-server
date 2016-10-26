280804122623d4f471ab62299d8713d4bf2f195d
Adam Belak <belakede@gmail.com>
2016-10-26 16:51:36 +0200

    Add findByRoom(String) and deleteByRoom(String) methods to AuthorRepository

--- suspect-note-server/src/main/java/me/belakede/thesis/server/note/repository/AuthorRepository.java
+++ suspect-note-server/src/main/java/me/belakede/thesis/server/note/repository/AuthorRepository.java
@@ -3,10 +3,18 @@ package me.belakede.thesis.server.note.repository;
 import me.belakede.thesis.server.note.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     Author findByNameAndRoom(String name, String room);
+
+    List<Author> findByRoom(String room);
+
+    @Transactional
+    Long deleteByRoom(String room);
 
 }
