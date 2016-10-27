3ee7c5ceaceb2853e3e8818f933be3ee97df14ea
belakede <belakede@gmail.com>
2016-10-27 22:37:34 +0200

    Add integration test for NoteController

--- suspect-note-server/src/test/java/me/belakede/thesis/server/note/controller/NoteControllerIntegrationTest.java
+++ suspect-note-server/src/test/java/me/belakede/thesis/server/note/controller/NoteControllerIntegrationTest.java
@@ -5,7 +5,9 @@ import me.belakede.test.security.oauth2.OAuth2Helper;
 import me.belakede.thesis.game.equipment.Marker;
 import me.belakede.thesis.game.equipment.Suspect;
 import me.belakede.thesis.server.auth.domain.Role;
-import me.belakede.thesis.server.note.domain.Author;
+import me.belakede.thesis.server.note.request.NoteRequest;
+import me.belakede.thesis.server.note.response.NoteResponse;
+import me.belakede.thesis.server.note.response.NotesResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,17 +19,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.web.context.WebApplicationContext;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 
 @Import(OAuth2Helper.class)
 @RunWith(OrderedSpringRunner.class)
@@ -40,47 +41,98 @@ public class NoteControllerIntegrationTest {
     @LocalServerPort
     private int randomServerPort;
     @Autowired
-    private WebApplicationContext context;
-    @Autowired
     private OAuth2Helper authHelper;
 
     private OAuth2AccessToken accessToken;
+    private OAuth2AccessToken testAccessToken;
+    private NoteRequest request;
 
     @Before
     public void setUp() throws Exception {
         accessToken = authHelper.createOAuth2AccessToken(adminUsername, Role.ADMIN.getAuthority());
+        testAccessToken = authHelper.createOAuth2AccessToken("testuser", Role.ADMIN.getAuthority());
+        request = new NoteRequest("test-room", Suspect.GREEN, "demo", Marker.YES);
     }
 
     @Test
     @Order(1)
-    public void joinShouldCreateAnAuthor() {
+    public void joinShouldCreateAnAuthorForAdmin() {
         Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes/join").queryParam("room", "test-room");
-        Author author = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes/join");
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
                 .header("Authorization", "Bearer " + accessToken.getValue())
-                .post(Entity.entity(null, "text/plain"), Author.class);
-        assertThat(author.getRoom(), is("test-room"));
-        assertThat(author.getName(), is(adminUsername));
-        assertThat(author.getNotes(), nullValue());
-        assertThat(author.getSuggestion(), nullValue());
+                .post(Entity.json(new NoteRequest("test-room")));
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
     }
 
     @Test
     @Order(2)
     public void storeShouldCreateANoteAndSaveForLater() {
         Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes")
-                .queryParam("room", "test-room")
-                .queryParam("card", Suspect.GREEN)
-                .queryParam("owner", "demo")
-                .queryParam("marker", Marker.YES);
-        webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes");
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
                 .header("Authorization", "Bearer " + accessToken.getValue())
-                .post(Entity.entity(null, "text/plain"));
+                .post(Entity.json(request));
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+    }
 
-//        assertThat(note.getCard(), is(Suspect.GREEN));
-//        assertThat(note.getOwner(), is("demo"));
-//        assertThat(note.getMarker(), is(Marker.YES));
+    @Test
+    @Order(3)
+    public void findAllShouldThrowMissingAuthorException() {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes").path("test-room");
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + testAccessToken.getValue())
+                .get();
+        assertThat(response.getStatus(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
+
+    @Test
+    @Order(4)
+    public void storeShouldThrowMissingAuthorException() {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes");
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + testAccessToken.getValue())
+                .post(Entity.json(request));
+        assertThat(response.getStatus(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
+
+    @Test
+    @Order(5)
+    public void joinShouldCreateAnAuthorForTestUser() {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes/join");
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + testAccessToken.getValue())
+                .post(Entity.json(new NoteRequest("test-room")));
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+    }
+
+    @Test
+    @Order(6)
+    public void findAllShouldReturnAnEmptyListIfUserHasNotCreateAnyNotes() {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes").path("test-room");
+        NotesResponse response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + testAccessToken.getValue())
+                .get(NotesResponse.class);
+        assertThat(response, not(nullValue()));
+        assertThat(response.getNotes().size(), is(0));
+        assertThat(response.getNotes(), not(contains(new NoteResponse(request.getCard(), request.getOwner(), request.getMarker()))));
+    }
+
+    @Test
+    @Order(7)
+    public void findAllShouldReturnAListWithAllPreviouslyStoredNote() {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:" + randomServerPort + "/notes").path("test-room");
+        NotesResponse response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + accessToken.getValue())
+                .get(NotesResponse.class);
+        assertThat(response, not(nullValue()));
+        assertThat(response.getNotes().size(), greaterThan(0));
+        assertThat(response.getNotes(), contains(new NoteResponse(request.getCard(), request.getOwner(), request.getMarker())));
     }
 
 
