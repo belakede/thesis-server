02103fb3d4d7f3d1bfda6ea013ba2ecd15b6754f
Adam Belak <belakede@gmail.com>
2016-10-27 16:55:47 +0200

    Add NoteServerControllerExceptionHandler

---
+++ suspect-note-server/src/main/java/me/belakede/thesis/server/note/configuration/NoteServerControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package me.belakede.thesis.server.note.configuration;
+
+import me.belakede.thesis.server.note.exception.MissingAuthorException;
+import me.belakede.thesis.server.note.exception.MissingSuggestionException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class NoteServerControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({MissingAuthorException.class, MissingSuggestionException.class})
+    public String handleException(Exception e) {
+        return e.getClass() + ": " + e.getMessage();
+    }
+
+}
