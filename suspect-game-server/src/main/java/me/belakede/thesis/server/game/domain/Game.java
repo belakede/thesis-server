615f8452aefceaace1872c15eb37af50ba22ad57
belakede <belakede@gmail.com>
2016-10-29 15:21:17 +0200

    Add and test Game

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/domain/Game.java
@@ -0,0 +1,105 @@
+package me.belakede.thesis.server.game.domain;
+
+import me.belakede.thesis.game.board.BoardType;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+public class Game implements Serializable {
+
+    private static final long serialVersionUID = 6620498616614285764L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true, updatable = false, nullable = false)
+    private String roomId;
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime created;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BoardType boardType;
+    @Enumerated(EnumType.STRING)
+    private GameStatus status;
+
+    public Game() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public BoardType getBoardType() {
+        return boardType;
+    }
+
+    public void setBoardType(BoardType boardType) {
+        this.boardType = boardType;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Game game = (Game) o;
+
+        return (roomId != null ? roomId.equals(game.roomId) : game.roomId == null)
+                && (created != null ? created.equals(game.created) : game.created == null)
+                && (boardType == game.boardType && status == game.status);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = roomId != null ? roomId.hashCode() : 0;
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        result = 31 * result + (boardType != null ? boardType.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", roomId='" + roomId + '\'' +
+                ", created=" + created +
+                ", boardType=" + boardType +
+                ", status=" + status +
+                '}';
+    }
+}
