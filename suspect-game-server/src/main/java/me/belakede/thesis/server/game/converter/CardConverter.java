43e0da975fc93e4165da2eedf0a38be62af58074
belakede <belakede@gmail.com>
2016-10-31 14:09:14 +0100

    Add CardConverter

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/CardConverter.java
@@ -0,0 +1,20 @@
+package me.belakede.thesis.server.game.converter;
+
+import me.belakede.thesis.game.equipment.Card;
+import me.belakede.thesis.internal.game.util.Cards;
+import me.belakede.thesis.server.game.domain.Player;
+import me.belakede.thesis.server.game.domain.PlayerCard;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardConverter {
+
+    public Card convert(PlayerCard card) {
+        return Cards.valueOf(card.getCard()).get();
+    }
+
+    public PlayerCard convert(Player player, Card card) {
+        return new PlayerCard(player, card.name());
+    }
+
+}
---
+++ suspect-game-server/src/test/java/me/belakede/thesis/server/game/converter/CardConverterTest.java
@@ -0,0 +1,42 @@
+package me.belakede.thesis.server.game.converter;
+
+import me.belakede.thesis.game.equipment.Card;
+import me.belakede.thesis.game.equipment.Room;
+import me.belakede.thesis.game.equipment.Weapon;
+import me.belakede.thesis.server.game.domain.Player;
+import me.belakede.thesis.server.game.domain.PlayerCard;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class CardConverterTest {
+
+    private CardConverter testSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        testSubject = new CardConverter();
+    }
+
+    @Test
+    public void testConvertShouldReturnACardBasedOnPlayerCard() throws Exception {
+        Player player = Mockito.mock(Player.class);
+        PlayerCard playerCard = new PlayerCard(player, Room.LIBRARY.name());
+        Card actual = testSubject.convert(playerCard);
+        assertThat(actual, notNullValue());
+        assertThat(actual, is(Room.LIBRARY));
+    }
+
+    @Test
+    public void testConvertShouldReturnAPlayerCardForTheSpecifiedPlayerFromACard() throws Exception {
+        Player player = Mockito.mock(Player.class);
+        PlayerCard actual = testSubject.convert(player, Weapon.ROPE);
+        assertThat(actual, notNullValue());
+        assertThat(actual.getCard(), is(Weapon.ROPE.name()));
+        assertThat(actual.getPlayer(), is(player));
+    }
+}
