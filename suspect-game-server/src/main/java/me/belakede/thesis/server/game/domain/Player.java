9853e706561ff3eba03d93175513869d3d2efdcb
belakede <belakede@gmail.com>
2016-10-31 13:27:25 +0100

    Add Player

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/domain/Player.java
@@ -0,0 +1,136 @@
+package me.belakede.thesis.server.game.domain;
+
+import me.belakede.thesis.game.equipment.Suspect;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(name = "PLAYER_UNIQUE_KEY", columnNames = {"game_id", "username"}))
+public class Player implements Serializable {
+    private static final long serialVersionUID = -5448865337444775717L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, updatable = false)
+    private Game game;
+
+    @Column(nullable = false, updatable = false)
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, updatable = false)
+    private Suspect figurine;
+
+    @Column(nullable = false)
+    private Boolean alive;
+
+    @Column(nullable = false)
+    private Integer ordinalNumber;
+
+    @Column(nullable = false)
+    private Boolean current;
+
+    public Player() {
+        // It's required for an entity
+    }
+
+    public Player(String username, Suspect figurine, Integer ordinalNumber) {
+        this(null, username, figurine, ordinalNumber);
+    }
+
+    public Player(Game game, String username, Suspect figurine, Integer ordinalNumber) {
+        this(game, username, figurine, ordinalNumber, true, false);
+    }
+
+    public Player(Game game, String username, Suspect figurine, Integer ordinalNumber, Boolean alive, Boolean current) {
+        this.game = game;
+        this.username = username;
+        this.figurine = figurine;
+        this.alive = alive;
+        this.ordinalNumber = ordinalNumber;
+        this.current = current;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Suspect getFigurine() {
+        return figurine;
+    }
+
+    public void setFigurine(Suspect figurine) {
+        this.figurine = figurine;
+    }
+
+    public Boolean getAlive() {
+        return alive;
+    }
+
+    public void setAlive(Boolean alive) {
+        this.alive = alive;
+    }
+
+    public Integer getOrdinalNumber() {
+        return ordinalNumber;
+    }
+
+    public void setOrdinalNumber(Integer ordinalNumber) {
+        this.ordinalNumber = ordinalNumber;
+    }
+
+    public Boolean getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Boolean current) {
+        this.current = current;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Player player = (Player) o;
+
+        return (game != null ? game.equals(player.game) : player.game == null)
+                && (username != null ? username.equals(player.username) : player.username == null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = game != null ? game.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        return result;
+    }
+}
---
+++ suspect-game-server/src/test/java/me/belakede/thesis/server/game/domain/PlayerTest.java
@@ -0,0 +1,38 @@
+package me.belakede.thesis.server.game.domain;
+
+import me.belakede.thesis.game.board.BoardType;
+import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.junit.ExtendedEqualsHashCodeTestCase;
+import me.belakede.thesis.junit.PojoClassTestCase;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
+@RunWith(Enclosed.class)
+public class PlayerTest {
+
+    private static final Game game = new Game(BoardType.ADVANCED, Game.Status.PAUSED);
+
+    public static final class PlayerEqualsHashCodeTest extends ExtendedEqualsHashCodeTestCase {
+        public PlayerEqualsHashCodeTest(String name) {
+            super(name, Player.class, Arrays.asList("game", "username"));
+        }
+
+        @Override
+        protected Player createInstance() throws Exception {
+            return new Player(game, "username", Suspect.PEACOCK, 1);
+        }
+
+        @Override
+        protected Player createNotEqualInstance() throws Exception {
+            return new Player(game, "otheruser", Suspect.GREEN, 2);
+        }
+    }
+
+    public static final class PlayerPojoTest extends PojoClassTestCase {
+        public PlayerPojoTest(String name) {
+            super(name, Player.class);
+        }
+    }
+}
