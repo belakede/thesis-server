bda80d1e804c1331da8f81086fa8f8d5d86123b5
belakede <belakede@gmail.com>
2016-11-01 20:17:41 +0100

    Fix GameRepository

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/repository/GameRepository.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/repository/GameRepository.java
@@ -1,6 +1,6 @@
 package me.belakede.thesis.server.game.repository;
 
-import me.belakede.thesis.game.Game;
+import me.belakede.thesis.server.game.domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
