f6ffc7525d7e1d22d9a7fcbbf2005b4229fc652b
belakede <belakede@gmail.com>
2016-11-01 20:24:02 +0100

    Add GameService TODO: Add option to auth-server to get user by name

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
@@ -0,0 +1,39 @@
+package me.belakede.thesis.server.game.service;
+
+import me.belakede.thesis.game.equipment.BoardType;
+import me.belakede.thesis.server.game.domain.Game;
+import me.belakede.thesis.server.game.repository.GameRepository;
+import me.belakede.thesis.server.game.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public GameService(GameRepository gameRepository, PlayerRepository playerRepository) {
+        this.gameRepository = gameRepository;
+        this.playerRepository = playerRepository;
+    }
+
+    public Game create(BoardType boardType, Collection<String> users) {
+        new Game(boardType, Game.Status.CREATED);
+        // TODO add option to get users by name from the auth server
+        return null;
+    }
+
+    public void remove(Long id) {
+        gameRepository.delete(id);
+    }
+
+    public List<Game> findAll() {
+        return gameRepository.findAll();
+    }
+
+}
