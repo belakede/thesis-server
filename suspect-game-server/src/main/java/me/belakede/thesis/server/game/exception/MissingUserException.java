1afff6539ebbd930e64221fb013307dd499b0a01
belakede <belakede@gmail.com>
2016-11-01 20:09:51 +0100

    Add MissingUserException

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/exception/MissingUserException.java
@@ -0,0 +1,8 @@
+package me.belakede.thesis.server.game.exception;
+
+public class MissingUserException extends Exception {
+
+    public MissingUserException(String message) {
+        super(message);
+    }
+}
