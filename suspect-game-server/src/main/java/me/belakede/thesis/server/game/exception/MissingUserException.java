c97bd7a5a598fd941aae0a8abf0c302ef9db6584
Adam Belak <belakede@gmail.com>
2016-11-02 10:15:48 +0100

    Remove duplicated MissingUserException

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/exception/MissingUserException.java
+++
@@ -1,8 +0,0 @@
-package me.belakede.thesis.server.game.exception;
-
-public class MissingUserException extends RuntimeException {
-
-    public MissingUserException(String message) {
-        super(message);
-    }
-}
