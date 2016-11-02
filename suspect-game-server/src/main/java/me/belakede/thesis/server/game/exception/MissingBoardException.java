5eabb71f7d8a8b3b926dc17b44d85c9216a84cd1
Adam Belak <belakede@gmail.com>
2016-11-02 10:49:35 +0100

    Add MissingBoardException

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/exception/MissingBoardException.java
@@ -0,0 +1,8 @@
+package me.belakede.thesis.server.game.exception;
+
+public class MissingBoardException extends RuntimeException {
+
+    public MissingBoardException(String message) {
+        super(message);
+    }
+}
