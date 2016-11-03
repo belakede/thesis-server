72410c4c773c839bf5e9dc453cfa0351abea92c1
Adam Belak <belakede@gmail.com>
2016-11-03 11:21:49 +0100

    Add MissingGameException to ExceptionHandler

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/configuration/GameServerControllerExceptionHandler.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/configuration/GameServerControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package me.belakede.thesis.server.game.configuration;
 
 import me.belakede.thesis.server.game.exception.MissingBoardException;
+import me.belakede.thesis.server.game.exception.MissingGameException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameServerControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({MissingBoardException.class})
+    @ExceptionHandler({MissingBoardException.class, MissingGameException.class})
     public String handleException(Exception e) {
         return e.getClass() + ": " + e.getMessage();
     }
