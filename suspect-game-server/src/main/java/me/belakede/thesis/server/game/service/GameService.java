c7327df0893987577ce3981edb1c8a44d25f37be
Adam Belak <belakede@gmail.com>
2016-11-03 11:44:04 +0100

    Move findById(Long) to LobbyService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
@@ -1,7 +1,5 @@
 package me.belakede.thesis.server.game.service;
 
-import me.belakede.thesis.server.game.domain.Game;
-import me.belakede.thesis.server.game.exception.MissingGameException;
 import me.belakede.thesis.server.game.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,16 +14,4 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
-    /**
-     * @param id
-     * @return
-     * @throws MissingGameException
-     */
-    public Game findById(Long id) {
-        Game game = gameRepository.findOne(id);
-        if (game == null) {
-            throw new MissingGameException("Game not found with id: " + id);
-        }
-        return game;
-    }
 }
