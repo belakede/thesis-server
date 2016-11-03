ed12ec3bdb9d2215af59ffd8a811c0a6d7870aeb
Adam Belak <belakede@gmail.com>
2016-11-03 16:28:14 +0100

    Improve PositionConverter

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/PositionConverter.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/PositionConverter.java
@@ -5,11 +5,18 @@ import me.belakede.thesis.server.game.response.Coordinate;
 import me.belakede.thesis.server.game.response.FigurineNotification;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 @Component
 public class PositionConverter {
 
     public FigurineNotification convert(Position position) {
         return new FigurineNotification(position.getFigurine(), new Coordinate(position.getRowIndex(), position.getColumnIndex()));
+    }
+
+    public Collection<FigurineNotification> convert(Collection<Position> positions) {
+        return positions.stream().map(this::convert).collect(Collectors.toList());
     }
 
 }
--- suspect-game-server/src/test/java/me/belakede/thesis/server/game/converter/PositionConverterTest.java
+++ suspect-game-server/src/test/java/me/belakede/thesis/server/game/converter/PositionConverterTest.java
@@ -1,12 +1,18 @@
 package me.belakede.thesis.server.game.converter;
 
 import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.game.equipment.Weapon;
 import me.belakede.thesis.server.game.domain.Position;
 import me.belakede.thesis.server.game.response.Coordinate;
 import me.belakede.thesis.server.game.response.FigurineNotification;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -27,6 +33,24 @@ public class PositionConverterTest {
 
         assertThat(actual.getFigurine(), is(Suspect.GREEN));
         assertThat(actual.getPosition(), is(new Coordinate(10, 12)));
+    }
+
+    @Test
+    public void testConvertShouldProduceACollectionOfFigurineNotificationFromAPositionList() {
+        List<Position> positions = Arrays.asList(
+                new Position(Suspect.GREEN.name(), 10, 12),
+                new Position(Suspect.MUSTARD.name(), 26, 3),
+                new Position(Weapon.CANDLESTICK.name(), 4, 16)
+        );
+
+        Collection<FigurineNotification> actual = testSubject.convert(positions);
+
+        assertThat(actual.size(), is(3));
+        assertThat(actual, containsInAnyOrder(
+                new FigurineNotification(Suspect.GREEN, new Coordinate(10, 12)),
+                new FigurineNotification(Suspect.MUSTARD, new Coordinate(26, 3)),
+                new FigurineNotification(Weapon.CANDLESTICK, new Coordinate(4, 16))
+        ));
     }
 
 }
