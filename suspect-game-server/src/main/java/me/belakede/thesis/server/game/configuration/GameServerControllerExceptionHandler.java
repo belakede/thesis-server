e8c0499ede98bd7c7f0d477edef5dabe51242381
Adam Belak <belakede@gmail.com>
2016-11-03 14:31:45 +0100

    Add InvalidUserConfiguration to exception handler

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/configuration/GameServerControllerExceptionHandler.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/configuration/GameServerControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package me.belakede.thesis.server.game.configuration;
 
+import me.belakede.thesis.server.game.exception.InvalidPlayerConfiguration;
 import me.belakede.thesis.server.game.exception.MissingBoardException;
 import me.belakede.thesis.server.game.exception.MissingGameException;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameServerControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({MissingBoardException.class, MissingGameException.class})
+    @ExceptionHandler({MissingBoardException.class, MissingGameException.class, InvalidPlayerConfiguration.class})
     public String handleException(Exception e) {
         return e.getClass() + ": " + e.getMessage();
     }
