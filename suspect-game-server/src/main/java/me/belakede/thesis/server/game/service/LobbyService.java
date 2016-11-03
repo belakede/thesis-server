887e273aa980aa6b0af8bfe51c65c1b9689c180d
Adam Belak <belakede@gmail.com>
2016-11-03 11:22:45 +0100

    Add findById() to LobbyService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/LobbyService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/LobbyService.java
@@ -8,6 +8,7 @@ import me.belakede.thesis.server.auth.service.UserService;
 import me.belakede.thesis.server.game.converter.GameConverter;
 import me.belakede.thesis.server.game.domain.Game;
 import me.belakede.thesis.server.game.exception.MissingBoardException;
+import me.belakede.thesis.server.game.exception.MissingGameException;
 import me.belakede.thesis.server.game.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -58,6 +59,19 @@ public class LobbyService {
 
     public List<Game> findAll() {
         return gameRepository.findAll();
+    }
+
+    /**
+     * @param id
+     * @return
+     * @throws MissingGameException
+     */
+    public Game findById(Long id) {
+        Game game = gameRepository.findOne(id);
+        if (game == null) {
+            throw new MissingGameException("Game not found with id: " + id);
+        }
+        return game;
     }
 
 }
