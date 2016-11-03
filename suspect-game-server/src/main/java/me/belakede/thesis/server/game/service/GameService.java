50e59093d28c44498da5aab1ba51caf9911a33fe
Adam Belak <belakede@gmail.com>
2016-11-03 11:46:05 +0100

    Add Game instance and a setter to GameService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
@@ -1,5 +1,6 @@
 package me.belakede.thesis.server.game.service;
 
+import me.belakede.thesis.server.game.domain.Game;
 import me.belakede.thesis.server.game.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,10 +9,15 @@ import org.springframework.stereotype.Service;
 public class GameService {
 
     private final GameRepository gameRepository;
+    private Game game;
 
     @Autowired
     public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
     }
 
 }
