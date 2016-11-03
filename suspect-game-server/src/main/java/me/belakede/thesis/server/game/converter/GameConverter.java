db42330239d64370e3310d23026ba5ebdda2518b
belakede <belakede@gmail.com>
2016-11-03 19:04:43 +0100

    Add convert() to produce game from entity

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/GameConverter.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/GameConverter.java
@@ -2,16 +2,22 @@ package me.belakede.thesis.server.game.converter;
 
 import me.belakede.thesis.game.Player;
 import me.belakede.thesis.game.equipment.BoardType;
+import me.belakede.thesis.game.equipment.Case;
+import me.belakede.thesis.game.equipment.Figurine;
 import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.internal.game.util.Coordinate;
+import me.belakede.thesis.internal.game.util.GameRebuilder;
 import me.belakede.thesis.server.game.domain.Game;
 import me.belakede.thesis.server.game.domain.Mystery;
 import me.belakede.thesis.server.game.domain.Position;
+import me.belakede.thesis.server.game.exception.MissingBoardException;
 import me.belakede.thesis.server.game.response.BoardStatus;
 import me.belakede.thesis.server.game.response.FigurineNotification;
 import me.belakede.thesis.server.game.response.GameStatusNotification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -63,6 +69,20 @@ public class GameConverter {
         Map<Suspect, String> suspectUsernameMap = new HashMap<>();
         players.forEach(p -> suspectUsernameMap.put(p.getFigurine(), p.getUsername()));
         return new GameStatusNotification(boardStatus, suspectUsernameMap);
+    }
+
+    public me.belakede.thesis.game.Game convert(Game game) {
+        BoardType boardType = game.getBoardType();
+        Case mystery = mysteryConverter.convert(game.getMystery());
+        List<me.belakede.thesis.server.game.domain.Player> players = game.getPlayers();
+        Collections.sort(players, (o1, o2) -> o1.getOrdinalNumber() - o2.getOrdinalNumber());
+        List<Player> logicPlayers = players.stream().map(playerConverter::convertToPlayer).collect(Collectors.toList());
+        Map<Figurine, Coordinate> positions = positionConverter.convertToMap(game.getPositions());
+        try {
+            return GameRebuilder.create().boardType(boardType).mystery(mystery).players(logicPlayers, logicPlayers.get(0)).positions(positions).build();
+        } catch (IOException e) {
+            throw new MissingBoardException("Board not found: " + boardType);
+        }
     }
 
 }
