25a5ad8c41c6ebadaaefeef721263e933995edd1
Adam Belak <belakede@gmail.com>
2016-11-03 14:31:08 +0100

    Add InvalidPlayerConfiguration

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/exception/InvalidPlayerConfiguration.java
@@ -0,0 +1,8 @@
+package me.belakede.thesis.server.game.exception;
+
+public class InvalidPlayerConfiguration extends RuntimeException {
+
+    public InvalidPlayerConfiguration(String message) {
+        super(message);
+    }
+}
