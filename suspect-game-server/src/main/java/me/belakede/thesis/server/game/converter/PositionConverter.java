d62862bb0ef57212691b97058270a0f02a48ef20
Adam Belak <belakede@gmail.com>
2016-11-03 16:20:48 +0100

    Change PositionConverter: convert Position to FigurineNotification

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/PositionConverter.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/converter/PositionConverter.java
@@ -1,35 +1,15 @@
 package me.belakede.thesis.server.game.converter;
 
-import me.belakede.thesis.game.board.Field;
-import me.belakede.thesis.game.equipment.Figurine;
-import me.belakede.thesis.internal.game.util.Coordinate;
-import me.belakede.thesis.internal.game.util.Figurines;
 import me.belakede.thesis.server.game.domain.Position;
+import me.belakede.thesis.server.game.response.Coordinate;
+import me.belakede.thesis.server.game.response.FigurineNotification;
 import org.springframework.stereotype.Component;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 public class PositionConverter {
 
-    public Map<Figurine, Coordinate> convert(Collection<Position> positions) {
-        Map<Figurine, Coordinate> coordinates = new HashMap<>(positions.size());
-        positions.forEach(p -> coordinates.put(Figurines.valueOf(p.getFigurine()).get(), convert(p)));
-        return coordinates;
-    }
-
-    public List<Position> convert(Map<Figurine, Field> fields) {
-        return fields.entrySet().stream()
-                .map(e -> new Position(e.getKey().name(), e.getValue().getRow(), e.getValue().getColumn()))
-                .collect(Collectors.toList());
-    }
-
-    private Coordinate convert(Position position) {
-        return new Coordinate(position.getRowIndex(), position.getColumnIndex());
+    public FigurineNotification convert(Position position) {
+        return new FigurineNotification(position.getFigurine(), new Coordinate(position.getRowIndex(), position.getColumnIndex()));
     }
 
 }
--- suspect-game-server/src/test/java/me/belakede/thesis/server/game/converter/PositionConverterTest.java
+++ suspect-game-server/src/test/java/me/belakede/thesis/server/game/converter/PositionConverterTest.java
@@ -1,22 +1,14 @@
 package me.belakede.thesis.server.game.converter;
 
-import me.belakede.thesis.game.board.Field;
-import me.belakede.thesis.game.equipment.Figurine;
 import me.belakede.thesis.game.equipment.Suspect;
-import me.belakede.thesis.game.equipment.Weapon;
-import me.belakede.thesis.internal.game.board.FieldFactory;
-import me.belakede.thesis.internal.game.util.Coordinate;
-import me.belakede.thesis.server.game.domain.Game;
 import me.belakede.thesis.server.game.domain.Position;
+import me.belakede.thesis.server.game.response.Coordinate;
+import me.belakede.thesis.server.game.response.FigurineNotification;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
-
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
 
 public class PositionConverterTest {
 
@@ -28,31 +20,13 @@ public class PositionConverterTest {
     }
 
     @Test
-    public void testConvertShouldProduceAPositionSetFromAFigurineFieldMap() {
-        Game game = mock(Game.class);
-        Map<Figurine, Field> coordinates = new HashMap<>();
-        coordinates.put(Suspect.WHITE, FieldFactory.getFieldBySymbol(10, 13, 'S'));
-        coordinates.put(Weapon.REVOLVER, FieldFactory.getFieldBySymbol(4, 4, 'R'));
+    public void testConvertShouldProduceAFigurineNotificationFromAPosition() {
+        Position position = new Position(Suspect.GREEN.name(), 10, 12);
 
-        List<Position> actual = testSubject.convert(coordinates);
-        actual.forEach(p -> p.setGame(game));
+        FigurineNotification actual = testSubject.convert(position);
 
-        assertThat(actual.size(), is(2));
-        assertThat(actual, containsInAnyOrder(new Position(Suspect.WHITE.name(), game, 10, 13),
-                new Position(Weapon.REVOLVER.name(), game, 4, 4)));
+        assertThat(actual.getFigurine(), is(Suspect.GREEN));
+        assertThat(actual.getPosition(), is(new Coordinate(10, 12)));
     }
 
-    @Test
-    public void testConvertShouldProduceAFigurineCoordinateMapFromAPositionSet() throws Exception {
-        Game game = mock(Game.class);
-        Set<Position> positions = new HashSet<>();
-        positions.add(new Position(Suspect.GREEN.name(), game, 10, 12));
-        positions.add(new Position(Weapon.LEAD_PIPE.name(), game, 24, 22));
-
-        Map<Figurine, Coordinate> actual = testSubject.convert(positions);
-
-        assertThat(actual.size(), is(2));
-        assertThat(actual.get(Suspect.GREEN), is(new Coordinate(10, 12)));
-        assertThat(actual.get(Weapon.LEAD_PIPE), is(new Coordinate(24, 22)));
-    }
 }
