1f538ae5e644cde79bf5fbbaa8178dfdca015b03
Adam Belak <belakede@gmail.com>
2016-11-04 13:42:49 +0100

    Add method to get GameStatusNotification

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameLogicService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameLogicService.java
@@ -1,28 +1,39 @@
 package me.belakede.thesis.server.game.service;
 
 import me.belakede.thesis.server.game.converter.GameConverter;
-import me.belakede.thesis.server.game.domain.Game;
+import me.belakede.thesis.server.game.domain.Player;
+import me.belakede.thesis.server.game.repository.PlayerRepository;
+import me.belakede.thesis.server.game.response.GameStatusNotification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class GameLogicService {
 
     private final GameConverter gameConverter;
+    private final PlayerRepository playerRepository;
     private me.belakede.thesis.game.Game gameLogic;
     private me.belakede.thesis.server.game.domain.Game gameEntity;
 
     @Autowired
-    public GameLogicService(GameConverter gameConverter) {
+    public GameLogicService(GameConverter gameConverter, PlayerRepository playerRepository) {
         this.gameConverter = gameConverter;
+        this.playerRepository = playerRepository;
     }
 
     public me.belakede.thesis.game.Game getGameLogic() {
         return gameLogic;
     }
 
-    public Game getGameEntity() {
+    public me.belakede.thesis.server.game.domain.Game getGameEntity() {
         return gameEntity;
+    }
+
+    public GameStatusNotification getGameStatusNotification() {
+        List<Player> players = playerRepository.findByGameOrderByOrdinalNumberAsc(gameEntity);
+        return gameConverter.convert(gameEntity, players);
     }
 
     public void setGame(me.belakede.thesis.server.game.domain.Game game) {
