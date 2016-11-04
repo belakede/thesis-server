71e09e550f8ad7873e5e0f2fc2c45c06a7fcca1c
Adam Belak <belakede@gmail.com>
2016-11-04 13:51:46 +0100

    Add RollService

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/RollService.java
@@ -0,0 +1,37 @@
+package me.belakede.thesis.server.game.service;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import me.belakede.thesis.game.equipment.PairOfDice;
+import me.belakede.thesis.server.game.response.PairOfDiceNotification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RollService {
+
+    private final GameLogicService gameLogicService;
+    private final NotificationService notificationService;
+    private final ObjectProperty<PairOfDice> pairOfDiceObjectProperty;
+
+    @Autowired
+    public RollService(GameLogicService gameLogicService, NotificationService notificationService) {
+        this.gameLogicService = gameLogicService;
+        this.notificationService = notificationService;
+        this.pairOfDiceObjectProperty = new SimpleObjectProperty<>();
+        hookupChangeListeners();
+    }
+
+    private void hookupChangeListeners() {
+        pairOfDiceObjectProperty.addListener((observable, oldValue, newValue) -> {
+            if (null != newValue) {
+                notificationService.broadcast(new PairOfDiceNotification(newValue.getFirst(), newValue.getSecond()));
+            }
+        });
+    }
+
+    public void update() {
+        pairOfDiceObjectProperty.setValue(gameLogicService.getGameLogic().roll());
+    }
+
+}
