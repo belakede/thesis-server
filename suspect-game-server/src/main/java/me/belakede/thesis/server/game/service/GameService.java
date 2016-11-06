6c8312ce1818bb0cbe4008633823324dc0eafac8
belakede <belakede@gmail.com>
2016-11-06 19:07:26 +0100

    Change GameService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameService.java
@@ -1,9 +1,85 @@
 package me.belakede.thesis.server.game.service;
 
+import me.belakede.thesis.game.equipment.Card;
+import me.belakede.thesis.game.equipment.Suspicion;
+import me.belakede.thesis.server.game.response.Coordinate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Service
 public class GameService {
 
+    private final GameLogicService gameLogicService;
+    private final JoinService joinService;
+    private final RollService rollService;
+    private final MoveService moveService;
+    private final SuspectService suspectService;
+    private final ShowService showService;
+    private final AccuseService accuseService;
+    private final PlayerService playerService;
+
+    @Autowired
+    public GameService(GameLogicService gameLogicService, JoinService joinService, RollService rollService, MoveService moveService, SuspectService suspectService, ShowService showService, AccuseService accuseService, PlayerService playerService) {
+        this.gameLogicService = gameLogicService;
+        this.joinService = joinService;
+        this.rollService = rollService;
+        this.moveService = moveService;
+        this.suspectService = suspectService;
+        this.showService = showService;
+        this.accuseService = accuseService;
+        this.playerService = playerService;
+    }
+
+    public void join(Principal principal) {
+        if (!gameLogicService.gameInProgress()) {
+            joinService.join(principal.getName());
+        }
+    }
+
+    public void roll(Principal principal) {
+        if (isCurrentPlayer(principal)) {
+            rollService.roll();
+        }
+    }
+
+    public void move(Principal principal, Coordinate coordinate) {
+        if (isCurrentPlayer(principal)) {
+            moveService.move(coordinate.getRow(), coordinate.getColumn());
+        }
+    }
+
+    public void suspect(Principal principal, Suspicion suspicion) {
+        if (isCurrentPlayer(principal)) {
+            suspectService.suspect(suspicion);
+        }
+    }
+
+    public void show(Principal principal, Card card) {
+        if (isNextPlayer(principal)) {
+            showService.show(card);
+        }
+    }
+
+    public void accuse(Principal principal, Suspicion suspicion) {
+        if (isCurrentPlayer(principal)) {
+            accuseService.accuse(suspicion);
+        }
+    }
+
+    public void exit(Principal principal) {
+        if (isCurrentPlayer(principal)) {
+            joinService.exit(principal.getName());
+        }
+    }
+
+    private boolean isCurrentPlayer(Principal principal) {
+        return principal.getName().equals(playerService.getCurrentPlayer().getUsername());
+    }
+
+    private boolean isNextPlayer(Principal principal) {
+        return principal.getName().equals(playerService.getNextPlayer().getUsername());
+    }
 
 }
