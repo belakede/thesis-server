8a6b71484e86370f345d1fba397e54462c22a291
belakede <belakede@gmail.com>
2016-11-06 16:48:09 +0100

    Add saveAndFlush() to GameLogicRepository

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameLogicService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/GameLogicService.java
@@ -2,6 +2,7 @@ package me.belakede.thesis.server.game.service;
 
 import me.belakede.thesis.server.game.converter.GameConverter;
 import me.belakede.thesis.server.game.domain.Player;
+import me.belakede.thesis.server.game.repository.GameRepository;
 import me.belakede.thesis.server.game.repository.PlayerRepository;
 import me.belakede.thesis.server.game.response.GameStatusNotification;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,15 @@ import java.util.List;
 public class GameLogicService {
 
     private final GameConverter gameConverter;
+    private final GameRepository gameRepository;
     private final PlayerRepository playerRepository;
     private me.belakede.thesis.game.Game gameLogic;
     private me.belakede.thesis.server.game.domain.Game gameEntity;
 
     @Autowired
-    public GameLogicService(GameConverter gameConverter, PlayerRepository playerRepository) {
+    public GameLogicService(GameConverter gameConverter, GameRepository gameRepository, PlayerRepository playerRepository) {
         this.gameConverter = gameConverter;
+        this.gameRepository = gameRepository;
         this.playerRepository = playerRepository;
     }
 
@@ -34,6 +37,10 @@ public class GameLogicService {
     public GameStatusNotification getGameStatusNotification() {
         List<Player> players = playerRepository.findByGameOrderByOrdinalNumberAsc(gameEntity);
         return gameConverter.convert(gameEntity, players);
+    }
+
+    public void saveAndFlush() {
+        gameRepository.saveAndFlush(gameEntity);
     }
 
     public void setGame(me.belakede.thesis.server.game.domain.Game game) {
