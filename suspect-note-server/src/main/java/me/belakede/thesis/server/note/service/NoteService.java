6d6b4e2e3f73fd8213469b7d2a53583c182f38ae
belakede <belakede@gmail.com>
2016-11-13 17:32:41 +0100

    Implement store(Note) to replace previous note if exists

--- suspect-note-server/src/main/java/me/belakede/thesis/server/note/service/NoteService.java
+++ suspect-note-server/src/main/java/me/belakede/thesis/server/note/service/NoteService.java
@@ -2,6 +2,7 @@ package me.belakede.thesis.server.note.service;
 
 import me.belakede.thesis.server.note.domain.Author;
 import me.belakede.thesis.server.note.domain.Note;
+import me.belakede.thesis.server.note.domain.Suggestion;
 import me.belakede.thesis.server.note.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,16 @@ public class NoteService {
 
     public Note create(Note note) {
         return noteRepository.save(note);
+    }
+
+    public Note store(Note note) {
+        Note stored = noteRepository.findByAuthorAndCardAndOwner(note.getAuthor(), note.getCard(), note.getOwner());
+        if (stored == null) {
+            stored = new Note(note.getAuthor(), note.getOwner(), note.getCard(), note.getMarker());
+        } else {
+            stored.setMarker(note.getMarker());
+        }
+        return noteRepository.save(stored);
     }
 
     public List<Note> findAllByAuthor(Author author) {
