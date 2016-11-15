54a9f4bb46efb6598437f0c12a53a50c20806758
Adam Belak <belakede@gmail.com>
2016-11-15 12:25:30 +0100

    Handle GameIsAlreadyRunningException

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/configuration/GameServerControllerExceptionHandler.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/configuration/GameServerControllerExceptionHandler.java
@@ -1,9 +1,6 @@
 package me.belakede.thesis.server.game.configuration;
 
-import me.belakede.thesis.server.game.exception.InvalidPlayerConfiguration;
-import me.belakede.thesis.server.game.exception.ItIsNotYourTurnException;
-import me.belakede.thesis.server.game.exception.MissingBoardException;
-import me.belakede.thesis.server.game.exception.MissingGameException;
+import me.belakede.thesis.server.game.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameServerControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({MissingBoardException.class, MissingGameException.class,
+    @ExceptionHandler({MissingBoardException.class, MissingGameException.class, GameIsAlreadyRunningException.class,
             InvalidPlayerConfiguration.class, ItIsNotYourTurnException.class})
     public String handleException(Exception e) {
         return e.getClass() + ": " + e.getMessage();
