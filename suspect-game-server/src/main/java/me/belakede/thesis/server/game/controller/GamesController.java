1832d6452f1e28362b4c9cfc870e7e5b6c045e79
Adam Belak <belakede@gmail.com>
2016-11-15 12:25:46 +0100

    Throw exception when game is already running

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/controller/GamesController.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/controller/GamesController.java
@@ -3,6 +3,7 @@ package me.belakede.thesis.server.game.controller;
 import me.belakede.thesis.game.equipment.Suspect;
 import me.belakede.thesis.server.game.domain.Game;
 import me.belakede.thesis.server.game.domain.Player;
+import me.belakede.thesis.server.game.exception.GameIsAlreadyRunningException;
 import me.belakede.thesis.server.game.request.GamesRequest;
 import me.belakede.thesis.server.game.response.GamesResponse;
 import me.belakede.thesis.server.game.service.GameLogicService;
@@ -49,8 +50,12 @@ public class GamesController {
 
     @RequestMapping(value = "/start/{id}", method = RequestMethod.POST)
     public void start(@PathVariable("id") Long id) {
-        Game game = lobbyService.findById(id);
-        gameLogicService.setGame(game);
+        if (gameLogicService.gameInProgress()) {
+            throw new GameIsAlreadyRunningException("A game is running. Please try later");
+        } else {
+            Game game = lobbyService.findById(id);
+            gameLogicService.setGame(game);
+        }
     }
 
     private Map<Suspect, String> createUsers(List<Player> players) {
