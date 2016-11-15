34aa630fcc6527572f67e80ef95c2df229dfd21b
Adam Belak <belakede@gmail.com>
2016-11-15 14:38:36 +0100

    Add findByStatus(Status) to GameRepository

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/repository/GameRepository.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/repository/GameRepository.java
@@ -4,6 +4,11 @@ import me.belakede.thesis.server.game.domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+
+    List<Game> findByStatus(Game.Status status);
+
 }
