961dc3dd5d6283fea68d366ff10f7602dcb5f843
belakede <belakede@gmail.com>
2016-11-17 21:00:17 +0100

    Add startHealthCheck() method to send heart beats

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/NotificationService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/NotificationService.java
@@ -1,6 +1,7 @@
 package me.belakede.thesis.server.game.service;
 
 import me.belakede.thesis.server.game.response.GamePausedNotification;
+import me.belakede.thesis.server.game.response.HeartBeatNotification;
 import me.belakede.thesis.server.game.response.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +29,7 @@ public class NotificationService {
     public NotificationService(GameLogicService gameLogicService) {
         this.gameLogicService = gameLogicService;
         this.emitters = new ConcurrentHashMap<>();
+        startHealthCheck();
     }
 
     public SseEmitter createEmitter(String username) {
@@ -81,6 +82,28 @@ public class NotificationService {
         emitters.clear();
     }
 
+    private void startHealthCheck() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                int sleepTime = 30 * 1000;
+                if (gameLogicService.gameInProgress() && !emitters.isEmpty()) {
+                    LOGGER.info("Sending heartbeat...");
+                    broadcast(new HeartBeatNotification());
+                    sleepTime = 10 * 1000;
+                }
+                LOGGER.info("Waiting {} seconds before next", sleepTime / 1000);
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    LOGGER.info("Interrupted sleep", e);
+                }
+            }
+        });
+        thread.setName("heartbeat");
+        thread.setDaemon(true);
+        thread.start();
+    }
+
     private void pauseGame() {
         gameLogicService.pauseTheGame();
         emitters.entrySet().forEach(es -> notifyPlayer(es.getKey(), new GamePausedNotification()));
@@ -94,8 +117,7 @@ public class NotificationService {
                 LOGGER.info("Notification for {} on channel {}: {}", user, emitter, notification);
                 emitter.send(notification, MediaType.APPLICATION_JSON);
                 sent = true;
-            } catch (IOException exception) {
-                emitters.get(user).complete();
+            } catch (Exception exception) {
                 LOGGER.warn("User {} is not available: {}", user, exception);
             }
         }
