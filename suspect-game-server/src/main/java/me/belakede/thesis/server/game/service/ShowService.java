f7b7479a2aa3f920b99895863b1f98a701f376d9
belakede <belakede@gmail.com>
2016-11-20 00:34:39 +0100

    Refactor ShowService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/ShowService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/ShowService.java
@@ -4,44 +4,55 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import me.belakede.thesis.game.equipment.Card;
 import me.belakede.thesis.server.game.response.CardNotification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ShowService {
+class ShowService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShowService.class);
+
+    private final ObjectProperty<Card> card = new SimpleObjectProperty<>();
     private final PlayerService playerService;
     private final NotificationService notificationService;
-    private final ObjectProperty<Card> cardObjectProperty;
 
     @Autowired
     public ShowService(PlayerService playerService, NotificationService notificationService) {
         this.playerService = playerService;
         this.notificationService = notificationService;
-        this.cardObjectProperty = new SimpleObjectProperty<>();
         hookupChangeListeners();
     }
 
-    public void show(Card card) {
-        cardObjectProperty.setValue(card);
+    void show(Card card) {
+        setCard(card);
+    }
+
+    private ObjectProperty<Card> cardProperty() {
+        return card;
+    }
+
+    private void setCard(Card card) {
+        this.card.set(card);
     }
 
     private void hookupChangeListeners() {
-        cardObjectProperty.addListener((observable, oldValue, newValue) -> {
+        cardProperty().addListener((observable, oldValue, newValue) -> {
+            LOGGER.debug("Shown card: {}", newValue);
             if (null != newValue) {
-                notificationService.broadcast(new CardNotification(true), getUsername(), getSpecialNotification(newValue));
+                broadcastShowing(newValue);
             } else {
-                notificationService.broadcast(new CardNotification(false));
+                broadcastNotShowing();
             }
         });
     }
 
-    private CardNotification getSpecialNotification(Card newValue) {
-        return new CardNotification(newValue.name());
+    private void broadcastNotShowing() {
+        notificationService.broadcast(new CardNotification(false));
     }
 
-    private String getUsername() {
-        return playerService.getCurrentPlayer().getUsername();
+    private void broadcastShowing(Card newValue) {
+        notificationService.broadcast(new CardNotification(true), playerService.getCurrentPlayer().getUsername(), new CardNotification(newValue.name()));
     }
-
 }
