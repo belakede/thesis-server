5f77c6f36f1a59e9e913fb7ccff233f3c2f0e1de
belakede <belakede@gmail.com>
2016-11-20 00:22:50 +0100

    Refactor NotificationService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/NotificationService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/NotificationService.java
@@ -1,117 +1,103 @@
 package me.belakede.thesis.server.game.service;
 
-import me.belakede.thesis.server.game.response.GamePausedNotification;
-import me.belakede.thesis.server.game.response.HeartBeatNotification;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.MapProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleMapProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener.Change;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import me.belakede.thesis.server.game.response.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-public class NotificationService {
+class NotificationService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
 
-    private final GameLogicService gameLogicService;
-    private final Map<String, SseEmitter> emitters;
+    private final MapProperty<String, SseEmitter> emitters = new SimpleMapProperty<>();
+    private final ListProperty<String> missingUsers = new SimpleListProperty<>();
 
-    @Autowired
-    public NotificationService(GameLogicService gameLogicService) {
-        this.gameLogicService = gameLogicService;
-        this.emitters = new ConcurrentHashMap<>();
-        startHealthCheck();
+    public NotificationService() {
+        setEmitters(FXCollections.observableMap(new ConcurrentHashMap<>(6)));
+        setMissingUsers(FXCollections.observableArrayList());
+        hookupChangeListeners();
     }
 
-    public SseEmitter createEmitter(String username) {
-        LOGGER.info("Creating emitter for {}", username);
-        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-        emitters.put(username, emitter);
-        return emitter;
+    SseEmitter openChannelForUser(String username) {
+        getEmitters().put(username, new SseEmitter(Long.MAX_VALUE));
+        return getEmitters().get(username);
     }
 
-    public void broadcast(Notification notification) {
+    void broadcast(Notification notification) {
         broadcast(notification, new HashMap<>());
     }
 
-    public void broadcast(Notification notification, String user, Notification specialNotification) {
+    void broadcast(Notification notification, String user, Notification specialNotification) {
         HashMap<String, Notification> specialNotifications = new HashMap<>(1);
         specialNotifications.put(user, specialNotification);
         broadcast(notification, specialNotifications);
     }
 
-    public void broadcast(Notification notification, Map<String, Notification> specialNotifications) {
-        List<String> missing = new ArrayList<>();
-        emitters.entrySet().forEach(entry -> {
-            Notification message = specialNotifications.containsKey(entry.getKey()) ? specialNotifications.get(entry.getKey()) : notification;
-            if (!notifyPlayer(entry.getKey(), message)) {
-                missing.add(entry.getKey());
-            }
-        });
-        LOGGER.info("Missing players: ", missing);
-        if (!missing.isEmpty()) {
-            LOGGER.info("Pausing game.");
-            pause();
-        }
-    }
-
-    public void notify(String user, Notification notification) {
+    void notify(String user, Notification notification) {
         if (!notifyPlayer(user, notification)) {
-            LOGGER.info("Can't notify player. Pausing game!");
-            pause();
+            getMissingUsers().add(user);
         }
     }
 
-    public void pause() {
-        pauseGame();
-        close();
+    void clear() {
+        getEmitters().values().forEach(ResponseBodyEmitter::complete);
+        getEmitters().clear();
+        missingUsersProperty().clear();
     }
 
-    public void close() {
-        LOGGER.info("Closing emitters...");
-        emitters.values().forEach(ResponseBodyEmitter::complete);
-        emitters.clear();
+    ObservableList<String> getMissingUsers() {
+        return missingUsers.get();
     }
 
-    private void startHealthCheck() {
-        Thread thread = new Thread(() -> {
-            while (true) {
-                int sleepTime = 30 * 1000;
-                if (gameLogicService.gameInProgress() && !emitters.isEmpty()) {
-                    LOGGER.debug("Sending heartbeat...");
-                    broadcast(new HeartBeatNotification());
-                    sleepTime = 5 * 1000;
-                }
-                LOGGER.debug("Waiting {} seconds before next", sleepTime / 1000);
-                try {
-                    Thread.sleep(sleepTime);
-                } catch (InterruptedException e) {
-                    LOGGER.info("Interrupted sleep", e);
+    private void setMissingUsers(ObservableList<String> missingUsers) {
+        this.missingUsers.set(missingUsers);
+    }
+
+    ListProperty<String> missingUsersProperty() {
+        return missingUsers;
+    }
+
+    private void hookupChangeListeners() {
+        missingUsers.addListener((Change<? extends String> change) -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    change.getAddedSubList().forEach(user -> {
+                        SseEmitter emitter = emitters.remove(user);
+                        if (emitter != null) {
+                            emitter.complete();
+                        }
+                    });
                 }
             }
         });
-        thread.setName("heartbeat");
-        thread.setDaemon(true);
-        thread.start();
     }
 
-    private void pauseGame() {
-        try {
-            gameLogicService.pauseTheGame();
-            emitters.entrySet().forEach(es -> notifyPlayer(es.getKey(), new GamePausedNotification()));
-        } finally {
-            emitters.clear();
-        }
+    private void broadcast(Notification notification, Map<String, Notification> specialNotifications) {
+        emitters.entrySet().forEach(entry -> notify(entry.getKey(), specialNotifications.containsKey(entry.getKey()) ? specialNotifications.get(entry.getKey()) : notification));
+    }
+
+    private ObservableMap<String, SseEmitter> getEmitters() {
+        return emitters.get();
+    }
+
+    private void setEmitters(ObservableMap<String, SseEmitter> emitters) {
+        this.emitters.set(emitters);
     }
 
     private boolean notifyPlayer(String user, Notification notification) {
@@ -119,18 +105,13 @@ public class NotificationService {
         if (emitters.containsKey(user)) {
             try {
                 SseEmitter emitter = emitters.get(user);
-                LOGGER.info("Notification for {} on channel {}: {}", user, emitter, notification);
+                LOGGER.info("Notification for {}: {}", user, notification);
                 emitter.send(notification, MediaType.APPLICATION_JSON);
                 sent = true;
-            } catch (IllegalStateException exception) {
-                LOGGER.info("Message send failed. Emitter is already set complete.");
-            } catch (IOException exception) {
-                LOGGER.info("An existing connection was forcibly closed by the remote host");
             } catch (Exception exception) {
-                LOGGER.warn("User {} is not available: {}", user, exception);
+                LOGGER.warn("User {} is not available.", user);
             }
         }
         return sent;
     }
-
 }
