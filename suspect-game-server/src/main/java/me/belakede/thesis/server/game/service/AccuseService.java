af47761e98d10308eea731d3bd8687bccc2d6ee9
belakede <belakede@gmail.com>
2016-11-20 00:14:57 +0100

    Redesign AccuseService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/AccuseService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/AccuseService.java
@@ -3,60 +3,98 @@ package me.belakede.thesis.server.game.service;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import me.belakede.thesis.game.equipment.Suspicion;
-import me.belakede.thesis.server.game.converter.CardConverter;
-import me.belakede.thesis.server.game.domain.Player;
 import me.belakede.thesis.server.game.response.AccusationNotification;
 import me.belakede.thesis.server.game.response.GameEndedNotification;
-import me.belakede.thesis.server.game.response.Notification;
-import me.belakede.thesis.server.game.response.PlayerOutNotification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccuseService {
+class AccuseService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccuseService.class);
+
+    private final ObjectProperty<Suspicion> suspicion = new SimpleObjectProperty<>();
+    private final GameService gameService;
     private final PlayerService playerService;
-    private final CardConverter cardConverter;
-    private final GameLogicService gameLogicService;
     private final PositionService positionService;
     private final NotificationService notificationService;
-    private final ObjectProperty<Suspicion> suspicionObjectProperty;
 
     @Autowired
-    public AccuseService(PlayerService playerService, CardConverter cardConverter, GameLogicService gameLogicService, PositionService positionService, NotificationService notificationService) {
+    public AccuseService(GameService gameService, PlayerService playerService, PositionService positionService, NotificationService notificationService) {
+        this.gameService = gameService;
         this.playerService = playerService;
-        this.cardConverter = cardConverter;
-        this.gameLogicService = gameLogicService;
         this.positionService = positionService;
         this.notificationService = notificationService;
-        this.suspicionObjectProperty = new SimpleObjectProperty<>();
         hookupChangeListeners();
     }
 
-    public void accuse(Suspicion suspicion) {
-        suspicionObjectProperty.setValue(suspicion);
+    void accuse(Suspicion suspicion) {
+        setSuspicion(suspicion);
     }
 
     private void hookupChangeListeners() {
-        suspicionObjectProperty.addListener((observable, oldValue, newValue) -> {
-            if (null != newValue) {
-                notificationService.broadcast(new AccusationNotification(newValue));
-                gameLogicService.getGameLogic().accuse(newValue);
-                positionService.update();
-                if (gameLogicService.getGameLogic().isGameEnded()) {
-                    gameLogicService.finishTheGame();
-                    notificationService.broadcast(new GameEndedNotification());
-                    notificationService.close();
-                } else {
-                    Player currentPlayer = playerService.killPlayer();
-                    notificationService.broadcast(getPlayerOutNotification(currentPlayer));
-                }
+        suspicionProperty().addListener((observable, oldValue, newValue) -> {
+            LOGGER.debug("Broadcasting accusation: {}", newValue);
+            broadcastAccusation(newValue);
+            updateGameLogic(newValue);
+            updatePositions();
+            if (isGameEnded()) {
+                LOGGER.debug("Finishing game");
+                broadcastGameIsEnded();
+                finishGame();
+                resetPlayerService();
+                resetNotificationService();
+            } else {
+                LOGGER.debug("Killing current player");
+                killCurrentPlayer();
             }
         });
     }
 
-    private Notification getPlayerOutNotification(Player currentPlayer) {
-        return new PlayerOutNotification(currentPlayer.getUsername(), cardConverter.convert(currentPlayer.getCards()));
+    private ObjectProperty<Suspicion> suspicionProperty() {
+        return suspicion;
+    }
+
+    private void setSuspicion(Suspicion suspicion) {
+        this.suspicion.set(suspicion);
+    }
+
+    private void killCurrentPlayer() {
+        playerService.kill();
+    }
+
+    private void resetNotificationService() {
+        notificationService.clear();
+    }
+
+    private void resetPlayerService() {
+        playerService.finish();
+    }
+
+    private void broadcastGameIsEnded() {
+        notificationService.broadcast(new GameEndedNotification());
+    }
+
+    private void finishGame() {
+        gameService.finishTheGame();
+    }
+
+    private boolean isGameEnded() {
+        return gameService.getGameLogic().isGameEnded();
+    }
+
+    private void updatePositions() {
+        positionService.update();
+    }
+
+    private void updateGameLogic(Suspicion newValue) {
+        gameService.getGameLogic().accuse(newValue);
+    }
+
+    private void broadcastAccusation(Suspicion newValue) {
+        notificationService.broadcast(new AccusationNotification(newValue));
     }
 
 }
