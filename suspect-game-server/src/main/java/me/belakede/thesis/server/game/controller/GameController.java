db82dd3480002ccf49db783d8205c651f76760aa
belakede <belakede@gmail.com>
2016-11-20 00:44:02 +0100

    Use GameManager instead of GameService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/controller/GameController.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/controller/GameController.java
@@ -6,7 +6,7 @@ import me.belakede.thesis.server.game.request.MoveRequest;
 import me.belakede.thesis.server.game.request.ShowRequest;
 import me.belakede.thesis.server.game.request.SuspectRequest;
 import me.belakede.thesis.server.game.response.Coordinate;
-import me.belakede.thesis.server.game.service.GameService;
+import me.belakede.thesis.server.game.service.GameManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,52 +23,50 @@ import java.security.Principal;
 public class GameController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GameController.class);
-    private final GameService gameService;
+    private final GameManager gameManager;
 
     @Autowired
-    public GameController(GameService gameService) {
-        this.gameService = gameService;
+    public GameController(GameManager gameManager) {
+        this.gameManager = gameManager;
     }
 
     @RequestMapping(value = "/join", method = RequestMethod.GET)
     public SseEmitter join(Principal principal) {
-        LOGGER.info("{} tries to join", principal.getName());
-        return gameService.join(principal);
+        return gameManager.join(principal);
     }
 
     @RequestMapping(value = "/leave", method = RequestMethod.POST)
     public void leave(Principal principal) {
-        LOGGER.info("{} lefts the game", principal.getName());
-        gameService.exit(principal);
+        gameManager.exit(principal);
     }
 
     @RequestMapping(value = "/roll", method = RequestMethod.POST)
     public void roll(Principal principal) {
-        gameService.roll(principal);
+        gameManager.roll(principal);
     }
 
     @RequestMapping(value = "/move", method = RequestMethod.POST)
     public void move(Principal principal, @RequestBody MoveRequest moveRequest) {
-        gameService.move(principal, new Coordinate(moveRequest.getRow(), moveRequest.getColumn()));
+        gameManager.move(principal, new Coordinate(moveRequest.getRow(), moveRequest.getColumn()));
     }
 
     @RequestMapping(value = "/next", method = RequestMethod.POST)
     public void next(Principal principal) {
-        gameService.next(principal);
+        gameManager.next(principal);
     }
 
     @RequestMapping(value = "/suspect", method = RequestMethod.POST)
     public void suspect(Principal principal, @RequestBody SuspectRequest suspectRequest) {
-        gameService.suspect(principal, new DefaultSuspicion(suspectRequest.getSuspect(), suspectRequest.getRoom(), suspectRequest.getWeapon()));
+        gameManager.suspect(principal, new DefaultSuspicion(suspectRequest.getSuspect(), suspectRequest.getRoom(), suspectRequest.getWeapon()));
     }
 
     @RequestMapping(value = "/show", method = RequestMethod.POST)
     public void show(Principal principal, @RequestBody ShowRequest showRequest) {
-        gameService.show(principal, Cards.valueOf(showRequest.getCard()).get());
+        gameManager.show(principal, Cards.valueOf(showRequest.getCard()).get());
     }
 
     @RequestMapping(value = "/accuse", method = RequestMethod.POST)
     public void accuse(Principal principal, @RequestBody SuspectRequest suspectRequest) {
-        gameService.accuse(principal, new DefaultSuspicion(suspectRequest.getSuspect(), suspectRequest.getRoom(), suspectRequest.getWeapon()));
+        gameManager.accuse(principal, new DefaultSuspicion(suspectRequest.getSuspect(), suspectRequest.getRoom(), suspectRequest.getWeapon()));
     }
 }
