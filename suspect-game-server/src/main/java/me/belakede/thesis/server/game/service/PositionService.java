dd49a3b13ca57161cc68d97264ce81f984054437
belakede <belakede@gmail.com>
2016-11-20 00:32:50 +0100

    Refactor PositionService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/PositionService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/PositionService.java
@@ -1,41 +1,65 @@
 package me.belakede.thesis.server.game.service;
 
+import javafx.beans.property.MapProperty;
+import javafx.beans.property.SimpleMapProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.MapChangeListener;
+import javafx.collections.MapChangeListener.Change;
 import javafx.collections.ObservableMap;
 import me.belakede.thesis.game.equipment.Figurine;
 import me.belakede.thesis.game.field.Field;
 import me.belakede.thesis.server.game.converter.PositionConverter;
 import me.belakede.thesis.server.game.domain.Position;
 import me.belakede.thesis.server.game.repository.PositionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PositionService {
+class PositionService {
 
-    private final GameLogicService gameLogicService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PositionService.class);
+
+    private final MapProperty<Figurine, Position> positions = new SimpleMapProperty<>();
+    private final GameService gameService;
     private final PositionConverter positionConverter;
     private final PositionRepository positionRepository;
     private final NotificationService notificationService;
-    private final ObservableMap<Figurine, Position> positions;
 
     @Autowired
-    public PositionService(GameLogicService gameLogicService, PositionConverter positionConverter, PositionRepository positionRepository, NotificationService notificationService) {
-        this.gameLogicService = gameLogicService;
+    public PositionService(GameService gameService, PositionConverter positionConverter, PositionRepository positionRepository, NotificationService notificationService) {
+        this.gameService = gameService;
         this.positionConverter = positionConverter;
         this.positionRepository = positionRepository;
         this.notificationService = notificationService;
-        this.positions = FXCollections.observableHashMap();
+        setPositions(FXCollections.observableHashMap());
         hookupChangeListeners();
     }
 
-    public void update() {
+    void update() {
         fetchUpdates();
     }
 
+    private void hookupChangeListeners() {
+        getPositions().addListener((Change<? extends Figurine, ? extends Position> change) -> {
+            if (change.wasAdded()) {
+                Position position = positionRepository.save(change.getValueAdded());
+                LOGGER.debug("{} position has been changed to {}", change.getKey(), change.getValueAdded());
+                notificationService.broadcast(positionConverter.convert(position));
+            }
+        });
+    }
+
+    private ObservableMap<Figurine, Position> getPositions() {
+        return positions.get();
+    }
+
+    private void setPositions(ObservableMap<Figurine, Position> positions) {
+        this.positions.set(positions);
+    }
+
     private void fetchUpdates() {
-        gameLogicService.getGameLogic().getPositions().forEach((figurine, field) -> {
+        gameService.getGameLogic().getPositions().forEach((figurine, field) -> {
             Position position = positions.containsKey(figurine)
                     ? changePosition(figurine, field)
                     : positionConverter.convert(figurine, field);
@@ -52,15 +76,4 @@ public class PositionService {
         }
         return position;
     }
-
-    private void hookupChangeListeners() {
-        positions.addListener((MapChangeListener.Change<? extends Figurine, ? extends Position> change) -> {
-            if (change.wasAdded()) {
-                Position position = change.getValueAdded();
-                positionRepository.save(position);
-                notificationService.broadcast(positionConverter.convert(position));
-            }
-        });
-    }
-
 }
