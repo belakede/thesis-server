e01e1dca2e4f8ad1de29cd0ecaf133e3e406b6c8
belakede <belakede@gmail.com>
2016-11-20 00:33:29 +0100

    Refactor RollService

--- suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/RollService.java
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/service/RollService.java
@@ -4,34 +4,43 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import me.belakede.thesis.game.equipment.PairOfDice;
 import me.belakede.thesis.server.game.response.PairOfDiceNotification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RollService {
+class RollService {
 
-    private final GameLogicService gameLogicService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RollService.class);
+
+    private final ObjectProperty<PairOfDice> pairOfDice = new SimpleObjectProperty<>();
+    private final GameService gameService;
     private final NotificationService notificationService;
-    private final ObjectProperty<PairOfDice> pairOfDiceObjectProperty;
 
     @Autowired
-    public RollService(GameLogicService gameLogicService, NotificationService notificationService) {
-        this.gameLogicService = gameLogicService;
+    public RollService(GameService gameService, NotificationService notificationService) {
+        this.gameService = gameService;
         this.notificationService = notificationService;
-        this.pairOfDiceObjectProperty = new SimpleObjectProperty<>();
         hookupChangeListeners();
     }
 
-    public void roll() {
-        pairOfDiceObjectProperty.setValue(gameLogicService.getGameLogic().roll());
+    void roll() {
+        setPairOfDice(gameService.getGameLogic().roll());
+    }
+
+    private ObjectProperty<PairOfDice> pairOfDiceProperty() {
+        return pairOfDice;
+    }
+
+    private void setPairOfDice(PairOfDice pairOfDice) {
+        this.pairOfDice.set(pairOfDice);
     }
 
     private void hookupChangeListeners() {
-        pairOfDiceObjectProperty.addListener((observable, oldValue, newValue) -> {
-            if (null != newValue) {
-                notificationService.broadcast(new PairOfDiceNotification(newValue.getFirst(), newValue.getSecond()));
-            }
+        pairOfDiceProperty().addListener((observable, oldValue, newValue) -> {
+            LOGGER.debug("Rolled: {}", newValue);
+            notificationService.broadcast(new PairOfDiceNotification(newValue.getFirst(), newValue.getSecond()));
         });
     }
-
 }
