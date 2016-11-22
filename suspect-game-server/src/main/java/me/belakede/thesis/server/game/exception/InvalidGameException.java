0e82ab5b440d341ecef013779a52061504c504b9
Adam Belak <belakede@gmail.com>
2016-11-22 13:42:01 +0100

    Add InvalidGameException

---
+++ suspect-game-server/src/main/java/me/belakede/thesis/server/game/exception/InvalidGameException.java
@@ -0,0 +1,7 @@
+package me.belakede.thesis.server.game.exception;
+
+public class InvalidGameException extends RuntimeException {
+    public InvalidGameException(String message) {
+        super(message);
+    }
+}
